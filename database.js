12b6273eeec68666c08abe4dc695eb7a19ce1cdd
ycabellop <y.cabello.ici@gmail.com>
2021-06-19 11:00:06 -0400

    DB fix

--- database.js
+++ database.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
-const MONGODB_URI = `${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+//const MONGODB_URI = `${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+//local:
+const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+
 
 console.log(MONGODB_URI);
 mongoose.connect(MONGODB_URI, {
