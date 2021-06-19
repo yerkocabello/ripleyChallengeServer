8654eb82fd65409b22c6283a6d13ee5371d5d550
ycabellop <y.cabello.ici@gmail.com>
2021-06-18 20:53:15 -0400

    DB fix

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
 const MONGODB_URI = `mongodb+srv://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 
-
+console.log(MONGODB_URI);
 mongoose.connect(MONGODB_URI, {
 	useUnifiedTopology: true,
 	useNewUrlParser: true
